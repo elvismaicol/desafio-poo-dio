54c31246cad931edd4b318853efc2cdde0f43a25
elvismaicol <elvismaicol@gmail.com>
2022-02-19 15:17:18 -0300

    inciando o projeto

---
+++ src/br/com/dio/desafio/main/Main.java
@@ -0,0 +1,31 @@
+package br.com.dio.desafio.main;
+
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Curso curso1 = new Curso();
+		curso1.setTitulo("curso java");
+		curso1.setDescrição("descrição curso java");
+		curso1.setCargaHoraria(8);
+				
+		Curso curso2 = new Curso();
+		curso2.setTitulo("curso js");
+		curso2.setDescrição("descrição curso js");
+		curso2.setCargaHoraria(4);
+		
+		Mentoria mentoria = new Mentoria();
+		
+		mentoria.setTitulo("Menstoria Java");
+		mentoria.setDescricao("Descrição mentoria Java");
+		mentoria.setData(LocalDate.now());
+
+		System.out.println(curso1);
+		System.out.println(curso2);
+	}
+}
